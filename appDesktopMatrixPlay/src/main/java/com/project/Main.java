91bd946f4424e6db6fecbfb55dd36647f7fbc669
monica438 <monica14dominguez@gmail.com>
2025-11-07 16:56:50 +0100

    new maven project + basic files

---
+++ appDesktopMatrixPlay/src/main/java/com/project/Main.java
@@ -0,0 +1,19 @@
+package com.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static UtilsWS wsClient;
+
+    public static void main(String[] args) {
+        // Iniciar app JavaFX   
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+    }
+}
